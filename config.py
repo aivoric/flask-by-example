60c6f5618c3165989e6d9e92c8ad52f75cd7fd78
Ivan Khokhlov <khokhlov.ivan@gmail.com>
2021-03-01 23:34:13 +0300

    Finished environment setup

---
+++ config.py
@@ -0,0 +1,30 @@
+"""
+We’ll have a base config class that the other config classes inherit from.
+"""
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
